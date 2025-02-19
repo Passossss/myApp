81e812a25157684e106a6b948329f4b9c6001468
gu-ubuntu <you@example.com>
2025-02-18 21:01:17 -0300

    page details

---
+++ lib/presentation/pages/detail/detail_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed() {},
+             child: const Text('Voltar',
+             style: TextStyle(
+              fontSize: 30,
+             )),
+        ),
+        ),
+        );
+  }
+  }
